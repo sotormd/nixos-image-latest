e2e00b36ac90f4022e7c5ece5f1437731a9ff665
vrm <sotormd@proton.me>
2025-12-29 18:32:19 +0530

    fix: remove zfs

--- latest-kernel.nix
+++ latest-kernel.nix
@@ -1,8 +1,15 @@
-{ pkgs, ... }:
+{ lib, pkgs, ... }:
 
 {
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
   # some zfs packages are broken
   nixpkgs.config.allowBroken = true;
+  boot.supportedFilesystems = lib.mkForce [
+    "ext4"
+    "vfat"
+    "ntfs"
+    "xfs"
+    "btrfs"
+  ];
 }
