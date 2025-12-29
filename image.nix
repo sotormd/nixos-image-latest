f11e43adad2dba1b4a84849d8bcdc8176fec7ef9
vrm <sotormd@proton.me>
2025-12-29 20:05:22 +0530

    feat: add flakes

--- image.nix
+++ image.nix
@@ -9,6 +9,11 @@
   time.timeZone = "UTC";
   nixpkgs.hostPlatform.system = "x86_64-linux";
 
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
+
   environment.systemPackages = with pkgs; [ nerd-fonts.fira-code ];
   services.desktopManager.gnome.extraGSettingsOverrides = ''
     [org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles/:aaaaaaaa-bbbb-cccc-dddd-eeeeeeeeeeee/]
